984bef77a7bfe7213ba09313077889f2e094ebf4
nestorrente <nestorrente@gmail.com>
2020-03-17 22:52:08 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUNDLE_HEADER = `
+EventBus v1.0.0
+https://github.com/nestorrente/event-bus
+
+Released under the MIT License.
+
+Build date: ${new Date().toISOString()}
+`.trim();
+
+const commonConfig = {
+	entry: './src/index.ts',
+	devtool: 'source-map',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			banner: BUNDLE_HEADER
+		})
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	}
+};
+
+const standaloneConfig = {
+	...commonConfig,
+	output: {
+		...commonConfig.output,
+		filename: `event-bus.js`,
+		library: 'EventBus',
+		libraryTarget: 'var',
+		libraryExport: 'default'
+	}
+};
+
+const moduleConfig = {
+	...commonConfig,
+	output: {
+		...commonConfig.output,
+		filename: `event-bus.esm.js`,
+	}
+};
+
+module.exports = [
+	standaloneConfig,
+	moduleConfig
+];
