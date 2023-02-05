feeee9fb8dc9d707501f772c95a5e2b645bc39ae
nestor.perezgonzalez <nestor.perezgonzalez@manomano.com>
2023-02-05 01:30:58 +0100

    Dependencies update.

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,5 +1,5 @@
 export interface Dictionary<T> {
     [key: string]: T;
 }
-export declare type Listener = (...args: any[]) => void;
+export type Listener = (...args: any[]) => void;
 //# sourceMappingURL=types.d.ts.map
